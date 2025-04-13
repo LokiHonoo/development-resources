91d658104523678532986a972dd109fa21a970e4
Loki Honoo <lokihonoo@hotmail.com>
2025-04-14 05:58:50 +0800

    #

---
+++ net40-windows/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace net40_windows
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ net8.0-windows/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace net8._0_windows
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
